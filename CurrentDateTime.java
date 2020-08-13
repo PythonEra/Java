053cdde96ca83092c030dde5a9959290c78f4a6b
kailas gorade <54238131+PythonEra@users.noreply.github.com>
2020-08-13 07:54:15 +0530

    Create CurrentDateTime.java

---
+++ CurrentDateTime.java
@@ -0,0 +1,9 @@
+//get Current date & time
+import java.time.LocalDateTime;
+
+public class CurrentDateTime {
+public static void main(String agrs[]) {
+	LocalDateTime current = LocalDateTime.now();
+	System.out.println("Current Date and Time "+current);
+}
+}
